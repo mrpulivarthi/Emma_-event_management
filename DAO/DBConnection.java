c71cca1bd1d3fd3bc81a615812e97fe902dd69bd
mrpulivarthi <pvenkat1469@gmail.com>
2025-03-14 09:28:01 -0400

    Create DBConnection.java

---
+++ DAO/DBConnection.java
@@ -0,0 +1,15 @@
+package com.event.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/event_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Vanaja@555";
+    
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
